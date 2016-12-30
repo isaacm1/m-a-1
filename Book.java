0094058e4bb38e339323cb522686e2f32a365362
isaacm1 <imereshian@yahoo.com>
2016-12-29 21:15:54 -0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,1 +1,21 @@
 "# m-a-1" 
+package auth;
+
+public class Book extends Product {
+	private Author author;
+	private String genre;
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getGenre() {
+		return genre;
+	}
+}
